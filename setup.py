8da7a4d7ae46be397f433fdd590ca69547f4000d
Matt Teter <mteter@upenn.edu>
2018-02-09 10:24:02 -0500

    2/9/18, update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup_bak.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',
+    version='1.1.1',
 
     description='Python script to scan Modbus devices',
     long_description=long_description,
